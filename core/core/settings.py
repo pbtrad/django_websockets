d8f3a06b61197962c5d9492563e5e0d27ef6306a
paul <paulbrowne2@gmail.com>
2022-02-03 14:28:49 +0000

    Added asgi settings and room view

--- core/core/settings.py
+++ core/core/settings.py
@@ -123,7 +123,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ASGI_APPLICATION = "core.asgi.application"
